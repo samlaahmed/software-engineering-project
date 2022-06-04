8274dac4d775d58233894f6bccf4d02908c1d89b
Hagar-abdelfattah <102551441+Hagar-abdelfattah@users.noreply.github.com>
2022-06-05 01:48:24 +0200

    Create pages.php
    about us

---
+++ pages.php
@@ -0,0 +1,16 @@
+<?php
+class Pages extends Controller
+{
+
+
+
+    public function about()
+    {
+        $viewPath = VIEWS_PATH . 'pages/About.php';
+        require_once $viewPath;
+        $aboutView = new About($this->getModel(), $this);
+        $aboutView->output();
+    }
+
+
+}

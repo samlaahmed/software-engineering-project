6eb2784769d890f32817b915e84cfef7cb17cddf
Mariam1904317 <96684192+Mariam1904317@users.noreply.github.com>
2022-06-05 01:41:18 +0200

    Add files via upload

---
+++ Pages.php
@@ -0,0 +1,106 @@
+<?php
+class Pages extends Controller
+{
+
+    public function index()
+    {
+        $viewPath = VIEWS_PATH . 'pages/Index.php';
+        require_once $viewPath;
+        $indexView = new Index($this->getModel(), $this);
+        $indexView->output();
+    }
+
+    public function about()
+    {
+        $viewPath = VIEWS_PATH . 'pages/About.php';
+        require_once $viewPath;
+        $aboutView = new About($this->getModel(), $this);
+        $aboutView->output();
+    }
+    public function search(){
+
+        $viewPath = VIEWS_PATH . 'pages/search.php';
+        require_once $viewPath;
+        $searchView = new search($this->getModel(), $this);
+        $searchView->output();
+
+    }
+    public function fetch(){
+
+        $viewPath = VIEWS_PATH . 'pages/fetch.php';
+        require_once $viewPath;
+        $fetchView = new fetch($this->getModel(), $this);
+        $fetchView->output();
+
+    }
+    public function admin(){
+
+        $viewPath = VIEWS_PATH . 'pages/admin.php';
+        require_once $viewPath;
+        $adminView = new admin($this->getModel(), $this);
+        $adminView->output();
+
+    }
+    public function appointments()
+    {
+        $viewPath = VIEWS_PATH . 'pages/appointments.php';
+        require_once $viewPath;
+        $appointmentsView = new appointments($this->getModel(), $this);
+        $appointmentsView ->output();
+    }
+    public function admin_view_admin(){
+
+        $viewPath = VIEWS_PATH . 'pages/admin_view_admin.php';
+        require_once $viewPath;
+        $admin_view_adminView = new admin_view_admin($this->getModel(), $this);
+        $admin_view_adminView->output();
+
+    }
+    public function admin_delete_admin(){
+
+        $viewPath = VIEWS_PATH . 'pages/admin_delete_admin.php';
+        require_once $viewPath;
+        $admin_delete_adminView = new admin_delete_admin($this->getModel(), $this);
+        $admin_delete_adminView->output();
+
+    }
+    public function admin_delete_doctor(){
+
+        $viewPath = VIEWS_PATH . 'pages/admin_delete_doctor.php';
+        require_once $viewPath;
+        $admin_delete_doctorView = new admin_delete_doctor($this->getModel(), $this);
+        $admin_delete_doctorView->output();
+
+    }
+    public function contact(){
+
+        $viewPath = VIEWS_PATH . 'pages/contact.php';
+        require_once $viewPath;
+        $contactView = new contact($this->getModel(), $this);
+        $contactView->output();
+
+    }
+    public function admin_view_messages(){
+
+        $viewPath = VIEWS_PATH . 'pages/admin_view_messages.php';
+        require_once $viewPath;
+        $admin_view_messagesView = new admin_view_messages($this->getModel(), $this);
+        $admin_view_messagesView->output();
+
+    }
+
+    public function admin_view_doctor(){
+
+        $viewPath = VIEWS_PATH . 'pages/admin_view_doctor.php';
+        require_once $viewPath;
+        $admin_view_doctorView = new admin_view_doctor($this->getModel(), $this);
+        $admin_view_doctorView->output();
+
+    }
+    
+    
+
+
+
+
+}

be58e2aa1becd8fb245550997330485d00b2ab5b
Mariam1904317 <96684192+Mariam1904317@users.noreply.github.com>
2022-06-05 01:48:33 +0200

    Update Treatments.php
    add more treatments

--- Treatments.php
+++ Treatments.php
@@ -78,6 +78,53 @@ EOT;
   </div>
   </div>
 
+<div style='margin-top: 3%' class='col-lg'>
+<div class='card' style='width: 30rem; left: 500px; '>
+
+  <img src='<?php echo URLROOT; ?>img/person/img7.jpeg' class='card-img-top' alt='...'>
+
+  <div class='card-body'>
+  <h5  class='card-title'> pedodontics </h5>
+  <p class='card-text'>
+  Paedodontics is the branch of dentistry that specialises in the care and management of children’s teeth.
+     Also known as paediatric dentistry, it treats patients from birth until adolescence.</p>
+  </div>
+  </div>
+  </div>
+  </div>
+
+<div style='margin-top: 3%' class='col-lg'>
+<div class='card' style='width: 30rem; left: 500px; '>
+
+  <img src='<?php echo URLROOT; ?>img/person/img11.jpeg' class='card-img-top' alt='...'>
+
+  <div class='card-body'>
+  <h5  class='card-title'>  Root Canal Therapy </h5>
+  <p class='card-text'>
+  It is the process of removing the nerves from the roots of a tooth.  A root canal is often necessary to save an infected tooth.
+    If an infected tooth is left alone for a long period of time, an abscess can form and can spread to the rest of the body. </p>
+  </div>
+  </div>
+  </div>
+  </div>
+
+<div style='margin-top: 3%' class='col-lg'>
+<div class='card' style='width: 30rem; left: 500px; '>
+
+  <img src='<?php echo URLROOT; ?>img/person/img8.jpeg' class='card-img-top' alt='...'>
+
+  <div class='card-body'>
+  <h5  class='card-title'> Gum Treatment
+ </h5>
+  <p class='card-text'> In addition to the meticulous cleaning, polishing, and examination of your teeth,
+     we also take the time to help our patients develop proper oral hygiene habits at home. 
+     We will evaluate your hygiene techniques and make adjustments to your routine where needed. 
+
+    </p>
+  </div>
+  </div>
+  </div>
+  </div>
 
 
 </html>
